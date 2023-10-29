81b04f3c3ac7da23a330165f75fb0fcbd292cf22
“elisejonkers” <elisejonkers18@hotmail.com>
2023-10-29 10:20:34 +0100

    interval of creating boxes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-class Game {
+/*class Game {
 
 
-}
+}*/
+
+
+
 
 class GreenBox {
     //position, size, functionalities
     constructor() {
         this.width = 15
         this.height = 15
-        this.positionX = 50
-        this.positionY = 50
+        /*  this.positionX = 50
+            this.positionY = 50 are these really necessary? */
 
         this.parentElement = document.getElementById("gameboard")
         this.newBox = document.createElement("div")
@@ -20,7 +23,7 @@ class GreenBox {
         this.newBox.style.width = this.width + 'vw'
         this.newBox.style.height = this.height + 'vh'
         this.newBox.style.left = (Math.random() * (100 - this.width) + 1) + 'vw'
-        this.newBox.style.bottom = (Math.random() * (100 - this.height) + 1) + 'vh'+
+        this.newBox.style.bottom = (Math.random() * (100 - this.height) + 1) + 'vh'
         this.newBox.style.backgroundColor = 'limegreen' // change this when they pop up randomly
 
         this.parentElement.appendChild(this.newBox)
@@ -28,7 +31,7 @@ class GreenBox {
     //console.log('check creating a abox')
     }
     clickAndRemove() {
-        
+      
     }
     
     appearOnScreen() {
@@ -36,22 +39,31 @@ class GreenBox {
 
     }
     disappearFromScreen() {
-        this.newBox.remove()
+        setTimeout(function(){
+            console.log('testing timer')
+        }, 3000)
+        
     }
 }
 
+const boxArray = []
 const box1 = new GreenBox()
-box1.createBox()
 
 
-//box1.disappearFromScreen()
+// setInterval(function(){
+//     const nextBox = new GreenBox()
+//     boxArray.push(nextBox)
+//     nextBox.createBox()
+// }, 3000) 
+
 
 
 const clickBox = document.querySelector('.greenbox')
 
-clickBox.addEventListener("click", e => {
+/*clickBox.addEventListener("click", e => {
     clickBox.remove()
-})
+}) */
+// So far, this only works for the first box
 
 
 
