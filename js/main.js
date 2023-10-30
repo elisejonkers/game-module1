bbc65035b2ba900fbc82038d3a55e3716383ab9b
“elisejonkers” <elisejonkers18@hotmail.com>
2023-10-30 11:12:19 +0100

    new classes and red random interval

--- js/main.js
+++ js/main.js
@@ -2,25 +2,28 @@ class Game {
     constructor() {
         this.width = 15
         this.height = 15
-
+    }
+    createBox() {
         this.parentElement = document.getElementById("gameboard")
         this.newBox = document.createElement("div")
-    }
-    createGreenBox() {
-        this.newBox.classList.add("greenbox")
-        this.newBox.classList.add("not-clicked")
 
         this.newBox.style.width = this.width + 'vw'
         this.newBox.style.height = this.height + 'vh'
         this.newBox.style.left = (Math.random() * (100 - this.width) + 1) + 'vw'
         this.newBox.style.bottom = (Math.random() * (100 - this.height) + 1) + 'vh'
-        this.newBox.style.backgroundColor = 'limegreen' // change this when they pop up randomly
 
         this.parentElement.appendChild(this.newBox)
     }
+}
+
+class Greenbox extends Game {
+    makeBoxGreen() {
+        this.newBox.classList.add("greenbox")
+        this.newBox.classList.add("not-clicked")
+        this.newBox.style.backgroundColor = 'limegreen' 
+    }
     clickGreen() {
-        const allGreenBoxes = document.querySelectorAll(".greenbox") // creates an array
-        //const allNotClicked = document.querySelectorAll(".not-clicked")
+        const allGreenBoxes = document.querySelectorAll(".greenbox") 
         allGreenBoxes.forEach(function (box) {
             box.addEventListener("click", function () {
                 box.style.display = "none"
@@ -28,7 +31,7 @@ class Game {
             })
         })
     }
-    gameOverFunction() {
+    gameOverGreen() {
         const allNotClicked = document.querySelectorAll(".not-clicked")
         if (allNotClicked.length > 0) {
             location.href = "./gameover.html"
@@ -36,53 +39,69 @@ class Game {
     }
 }
 
-class Redbox {
-    constructor() {
-        this.width = 15
-        this.height = 15
-
-        this.parentElement = document.getElementById("gameboard")
-        this.newBox = document.createElement("div")
-    }
-    createRedBox() {
+class Redbox extends Game {
+    makeBoxRed() {
         this.newBox.classList.add("redbox")
-
-        this.newBox.style.width = this.width + 'vw'
-        this.newBox.style.height = this.height + 'vh'
-        this.newBox.style.left = (Math.random() * (100 - this.width) + 1) + 'vw'
-        this.newBox.style.bottom = (Math.random() * (100 - this.height) + 1) + 'vh'
-        this.newBox.style.backgroundColor = 'red' // change this when they pop up randomly
-
-        this.parentElement.appendChild(this.newBox)
+        this.newBox.style.backgroundColor = 'red' 
     }
-    clickRed () {
-        const allRedBoxes = document.querySelectorAll(".redbox") // creates an array
+    clickRed() {
+        const allRedBoxes = document.querySelectorAll(".redbox") 
         allRedBoxes.forEach(function (box) {
             box.addEventListener("click", function () {
-                //console.log('this is a click on a red box')
                 location.href = "./gameover.html"
             })
         })
     }
+    removeRed() {
+        this.newBox.remove()
+    }
 }
 
-const boxArray = [] // do I need this one?
-const box1 = new Game()
-// const box2 = new Game()
-const redBox1 = new Redbox()
-redBox1.createRedBox()
-redBox1.clickRed()
-//box1.createGreenBox()
+// const greenbox1 = new Greenbox()
+// greenbox1.createBox()
+// greenbox1.createGreenBox()
+// greenbox1.clickGreen()
 
-setInterval(function(){
-    const startGame = new Game()
-    startGame.createGreenBox()
-    startGame.clickGreen()
-    setTimeout(function(){
-        startGame.gameOverFunction()
-    },2400)
-}, 2500) 
+// const redbox1 = new Redbox()
+// redbox1.createBox()
+// redbox1.makeBoxRed()
+// redbox1.clickRed()
+// setTimeout(function (){
+//     redbox1.removeRed()
+// }, 2400)
 
-// De interval moet gaan kiezen tussen rood of groen
-// Wellicht random nummer kiezen, indien < 0,5 rood en indien > 0,5 groen
-// Code verkorten: maak een game class, extend met groen en rood --> functie van een box aanmaken in de game, extend rood en extend groen met de specifieke functies
+
+setInterval(function () {
+    const startGameGreen = new Greenbox()
+    startGameGreen.createBox()
+    startGameGreen.makeBoxGreen()
+    startGameGreen.clickGreen()
+    setTimeout(function () {
+        startGameGreen.gameOverGreen()
+    }, 2400)
+}, 2500)
+
+
+function getRandomSeconds(min, max) {
+    return (Math.random() * (max - min + 1) + min)
+}
+
+function intervalRandomSeconds() {
+    const randomSeconds = getRandomSeconds(1000, 15000)
+
+    setTimeout(function () {
+        const startGameRed = new Redbox()
+        startGameRed.createBox()
+        startGameRed.makeBoxRed()
+        startGameRed.clickRed()
+
+        intervalRandomSeconds()
+        setTimeout(function () {
+            startGameRed.removeRed()
+        }, 2400)
+    }, randomSeconds)
+}
+
+intervalRandomSeconds()
+
+
